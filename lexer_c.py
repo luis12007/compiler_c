ce1e25a4abbf5355490326a4510c6c0048bf16ab
Gabriel Martinez <gabriel.emv411@gmail.com>
2024-11-12 20:33:16 -0600

    Add: New Lexer

--- lexer_c.py
+++ lexer_c.py
@@ -46,53 +46,43 @@ palabras_reservadas = {
 
 # Dictionary for operators
 operadores = {
-    '=': 'Asignacion',
-    '==': 'Igualdad',
-    '!=': 'Diferente de',
-    '+': 'Operador suma',
-    '-': 'Operador resta',
+    '=': 'Igual',
+    '+': 'suma',
+    '-': 'resta',
     '*': 'Multiplicacion',
     '/': 'Division',
-    '++': 'Operador incremento',
-    '--': 'Operador decremento',
     '%': 'Modulo',
     '>': 'Mayor que',
     '<': 'Menor que',
-    '>=': 'Mayor o igual que',
-    '<=': 'Menor o igual que',
-    '&&': 'AND logico',
-    '||': 'OR logico',
-    '!': 'Negacion logica',
-    '&': 'AND bit a bit',
-    '|': 'OR bit a bit',
-    '^': 'XOR bit a bit',
-    '~': 'Complemento',
-    '<<': 'Desplazamiento a la izquierda',
-    '>>': 'Desplazamiento a la derecha',
-    '+=': 'Asignacion suma',
-    '-=': 'Asignacion resta',
-    '*=': 'Asignacion multiplicacion',
-    '/=': 'Asignacion division'
+    '&': 'Ampersand',
+    '|': 'Pipe',
+    '!': 'Negacion',
+    '^': 'Potencia',
+    '~': 'Complemento'
 }
 
 # Dictionary for delimiters
-delimitadores = {
+especiales = {
     '.': 'Punto',
     ';': 'Punto y coma',
     ',': 'Coma',
     ':': 'Dos puntos',
-    '(': 'Inicio de paréntesis',
-    ')': 'Fin de paréntesis',
-    '{': 'Inicio de llave',
-    '}': 'Fin de llave',
-    '[': 'Inicio de corchete',
-    ']': 'Fin de corchete',
-    '#': 'Inicio de directiva',
     '<': 'Menor que',
     '>': 'Mayor que'
 }
 
-import re
+aperturas = {
+    '(': 'Inicio de paréntesis',
+    '{': 'Inicio de llave',
+    '[': 'Inicio de corchete',
+}
+
+cerraduras = {
+    ')': 'Fin de paréntesis',
+    '}': 'Fin de llave',
+    ']': 'Fin de corchete',
+}
+
 class Token:
     def __init__(self, tipo, valor, linea):
         self.tipo = tipo
@@ -104,7 +94,7 @@ class Token:
 
 def lexer(codigo):
     tokens = []
-    current_token = ''
+    current_token = ""
     i = 0
     linea = 1
 
@@ -118,13 +108,13 @@ def lexer(codigo):
             continue
 
         # Handle single-line comments
-        if char == '/' and i + 1 < len(codigo) and codigo[i + 1] == '/':
+        elif char == '/' and i + 1 < len(codigo) and codigo[i + 1] == '/':
             while i < len(codigo) and codigo[i] != '\n':
                 i += 1
             continue
 
         # Handle multi-line comments
-        if char == '/' and i + 1 < len(codigo) and codigo[i + 1] == '*':
+        elif char == '/' and i + 1 < len(codigo) and codigo[i + 1] == '*':
             i += 2
             while i < len(codigo) - 1 and not (codigo[i] == '*' and codigo[i + 1] == '/'):
                 if codigo[i] == '\n':
@@ -134,80 +124,46 @@ def lexer(codigo):
             continue
 
         # Ignore whitespace
-        if char.isspace():
+        elif char.isspace():
             i += 1
             continue
 
-        
-        # Handle #include directive with <...> pattern for libraries
-        if codigo[i:i + 8] == '#include':
-            tokens.append(Token('Directiva de Inclusion', '#include', linea))
-            i += 9  # Move past '#include'
-
-            # Capture the content within <...> as a single ID token
-            if i < len(codigo) and codigo[i] == '<':
-                tokens.append(Token('Menor que', '<', linea))
-                i += 1  # Move past '<'
-                header_name = ''
-
-                while i < len(codigo) and codigo[i] != '>':
-                    
-                    header_name += codigo[i]
-                    i += 1
-
-                if i < len(codigo) and codigo[i] == '>':
-                    # Include '<' and '>' around the ID for full pattern representation
-                    tokens.append(Token( f'{header_name}' , 'VARNAME',linea))
-                    i += 1  # Move past '>'
-                tokens.append(Token('Mayor que', '>', linea))
-            continue
-
-
-        # Handle keywords, VARNAME, and general ID
-        if char.isalpha() or char == '_':
-            while i < len(codigo) and (codigo[i].isalnum() or codigo[i] == '_'):
-                current_token += codigo[i]
-                i += 1
-
-            if re.match(r"^[a-zA-Z_][a-zA-Z0-9_]*\.[a-zA-Z0-9]+$", current_token):
-                tokens.append(Token('ID' ,current_token, linea))  # Specific library names, etc.
-            else:
-                if current_token in palabras_reservadas:
-                    tokens.append(Token("VARNAME", current_token, linea))
+        else:
+            append = []
+            while(not char.isspace()):
+                if(char in especiales.keys()):
+                    if current_token != "":
+                        append.append(Token("VARNAME", current_token, linea))
+                    append.append(Token(especiales[char], char, linea))
+                    current_token = ""
+                elif(char in aperturas.keys()):
+                    if current_token != "":
+                        append.append(Token("VARNAME", current_token, linea))
+                    append.append(Token(aperturas[char], char, linea))
+                    current_token = ""
+                elif(char in operadores.keys()):
+                    if current_token != "":
+                        append.append(Token("VARNAME", current_token, linea))
+                    append.append(Token(operadores[char], char, linea))
+                    current_token = ""
+                elif(char in cerraduras.keys()):
+                    if current_token != "":
+                        append.append(Token("VARNAME", current_token, linea))
+                    append.append(Token(cerraduras[char], char, linea))
+                    current_token = ""
                 else:
-                    tokens.append(Token(current_token, 'VARNAME', linea))  # General variable names
-            current_token = ''
-            continue
-
-        # Handle multi-character operators (==, !=, etc.)
-        if char in operadores.keys():
-            next_char = codigo[i + 1] if i + 1 < len(codigo) else ''
-            if char + next_char in operadores:
-                tokens.append(Token(operadores[char + next_char], char + next_char, linea))
-                i += 2
-            else:
-                tokens.append(Token(operadores[char], char, linea))
+                    current_token += char  
                 i += 1
-            continue
-
-        # Handle delimiters
-        if char in delimitadores:
-            tokens.append(Token(delimitadores[char], char, linea))
+                char = codigo[i]
+                if(char == '\n'):
+                    linea += 1
+            if current_token != "":
+                append.append(Token("VARNAME", current_token, linea))
+            #print (append)
+            for item in append:
+                tokens.append(item)        
             i += 1
-            continue
-
-        # Handle numbers
-        if char.isdigit():
-            while i < len(codigo) and codigo[i].isdigit():
-                current_token += codigo[i]
-                i += 1
-            tokens.append(Token('NUMERO', current_token, linea))
-            current_token = ''
-            continue
-
-        # Handle unrecognized characters as errors
-        tokens.append(Token('ERROR', char, linea))
-        i += 1
+            current_token = ""
 
     return tokens
 
@@ -222,7 +178,6 @@ def imprimir_tabla(tokens):
 
 
 
-""" Working var table """
 class Var:
     def __init__(self, name, value):
         self.name = name
@@ -233,7 +188,7 @@ class Var:
     
 
 
- #TODO: change it
+#TODO: change it
 def trabajar_variables(tokens):
     length = len(tokens)
     variables = {}
@@ -292,3 +247,10 @@ def imprimir_variables(variables):
     print(tabla)
 
 
+# Leer el archivo example_one.cpp
+with open('example_one.c', 'r') as content:
+    codigo = content.read()
+# Ejecutar el lexer
+tokens = lexer(codigo)
+# Imprimir la tabla de símbolos
+imprimir_tabla(tokens)
